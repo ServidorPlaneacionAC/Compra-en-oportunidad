f4d3a14559c26b5ec6824dc77771fcd88c024058
ServidorPlaneacionAC <132586180+ServidorPlaneacionAC@users.noreply.github.com>
2023-06-23 16:04:20 -0500

    prueba

--- ModeloCompraOportunidad.py
+++ ModeloCompraOportunidad.py
@@ -123,6 +123,8 @@ if data_file is not None:
         inventarioMinimo = st.slider("Inventario Minimo", 2, 40, 2)
         LeadTime = st.slider("Lead Time", 2, 40, 1)
         
+        demanda[:5,0]
+
         # Cumplir con politicas de Inventario
 
         for m in materiales:
